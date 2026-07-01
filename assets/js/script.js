051f331c6df1e64c338492fc7ecdc9da0a0404c8
GaikwadSampada <gaikwadsampada2812@gmail.com>
2026-07-01 16:18:06 +0530

    Add EmailJs

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,3 +48,64 @@ if (navLinks.classList.contains("active")) {
     menuIcon.classList.add("fa-bars");
 
 }
+
+// ==============================
+// EMAIL JS
+// ==============================
+
+emailjs.init({
+    publicKey: "VI_M5Aj2ybDnIyWwB"
+});
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const formData = {
+
+        name: this.name.value,
+
+        email: this.email.value,
+
+        subject: this.subject.value,
+
+        message: this.message.value,
+
+        time: new Date().toLocaleString()
+
+    };
+
+ emailjs.send(
+    "service_tq9y59a",
+    "template_0lrt2ps",
+    formData,
+    "VI_M5Aj2ybDnIyWwB"
+)
+
+  .then(() => {
+
+    const success = document.getElementById("successMessage");
+
+    success.style.display = "block";
+    success.textContent = "✅ Message Sent Successfully!";
+
+    contactForm.reset();
+
+    setTimeout(() => {
+        success.style.display = "none";
+    }, 3000);
+
+})
+
+  .catch((error) => {
+
+    console.log(error);
+    console.log(error.status);
+    console.log(error.text);
+
+    alert("Failed to send");
+
+});
+
+});
