4ad6c141ed86c994ba8e611adef5f17de9dd72c7
GaikwadSampada <gaikwadsampada2812@gmail.com>
2026-07-01 12:29:09 +0530

    Fix mobile navigation toggle logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,50 @@
-
 const menuBtn = document.getElementById("menuBtn");
-
+const menuIcon = document.getElementById("menuIcon");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
 
     navLinks.classList.toggle("active");
 
+    if (navLinks.classList.contains("active")) {
+
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+
+    } else {
+
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+
+    }
 });
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach((item) => {
+
+    item.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+
+    });
+
+});
+if (navLinks.classList.contains("active")) {
+
+    document.body.style.overflow = "hidden";
+
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
+
+} else {
+
+    document.body.style.overflow = "auto";
+
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+
+}
